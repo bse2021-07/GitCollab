c4f53b2534a4efa11e85a60362f7c2148121a46d
Beula Samuel <beulathamban@gmail.com>
2021-10-21 09:42:07 +1300

    Initial commit with headers

---
+++ GitCollab/GitCollab.cpp
@@ -0,0 +1,20 @@
+// GitCollab.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+//structure
+
+
+//function prototype
+
+int main()
+{
+    //Beula
+    cout << "\nThis is from Beula";
+}
+
+//functions
